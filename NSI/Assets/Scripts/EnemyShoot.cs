c7cf79903e701be03226285e97c8d8679808a7d8
Paul Davis <C16324311@mydit.ie>
2018-04-12 16:13:59 +0100

    created enemy bullet and now user and enemy can kill each other

---
+++ NSI/Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+
+    public float timeBetweenShots;
+    public float nextShot = -1;
+    public GameObject bullet;
+
+    // Use this for initialization
+    void Start()
+    {
+        nextShot = Random.Range(1, 6.0f);
+        timeBetweenShots = Random.Range(3, 9f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextShot)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            nextShot = Time.time + timeBetweenShots;
+        }
+    }
+}
---
+++ NSI/Assets/Scripts/ShotControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotControl : MonoBehaviour
+{
+
+    public int speed = 3;
+    public int damage;
+    public string target;
+    public GameObject explosion;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == target)
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
